60ac3909c06953035f15b31740a5830f9488fd13
LEXX <podtopkinalexei@gmail.com>
2025-11-20 00:02:45 +0500

    Init commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Сервис добавления эмодзи по центру в видео</h1>
+    <VideoProcessor />
+  </div>
+</template>
+
+<script>
+import VideoProcessor from './components/VideoProcessor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VideoProcessor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
